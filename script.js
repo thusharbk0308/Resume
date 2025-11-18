d189d434aed64f56d001ce15a6ec002c279a59c0
thusharbk0308 <thusharbk0308@gmail.com>
2025-11-19 00:52:00 +0530

    Updated website with nav bar, links and styling

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// Button scrolls back to top
-document.getElementById("topBtn").onclick = function() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
+// Show "Go to Top" button on scroll
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 200) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+// Scroll back to top when button pressed
+topBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
