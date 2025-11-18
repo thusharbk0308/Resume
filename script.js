c3eb2f18f636a19793c408d6ec5c4fccafbd91db
thusharbk0308 <thusharbk0308@gmail.com>
2025-11-19 01:03:24 +0530

    Updated contact links and colors

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-// Show "Go to Top" button on scroll
 let topBtn = document.getElementById("topBtn");
 
 window.onscroll = function () {
-  if (document.documentElement.scrollTop > 200) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
-  }
+    if (document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
 };
 
-// Scroll back to top when button pressed
-topBtn.onclick = function () {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-};
+topBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
